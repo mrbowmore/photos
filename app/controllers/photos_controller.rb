f6796de8019e842ee1d247e0cb389607dc94817c
Kristoffer <kristoffer@tassadar.local>
2009-02-12 19:43:16 +0100

    Initial commit

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,40 @@
+class PhotosController < ApplicationController
+  
+  def index
+    @photos = Photo.all(:conditions => { :parent_id => nil })
+  end
+  
+  def new
+    @photo = Photo.new
+  end
+  
+  def create
+    album = Album.find(params[:album_id])
+    @photo = Photo.new(params[:photo])
+    @photo.album = album
+    
+    if @photo.save
+      flash[:notice] = "Photo was successfully saved!"
+      redirect_to @photo
+    else
+      flash[:notice] = "File size must be between 0->1024 KB"
+      render :action => :new
+    end
+  end
+  
+  def show
+    @photo = Photo.find(params[:id])
+  end
+  
+  def destroy
+    _find_album
+    @photo = @album.photos.find(params[:id])
+    @photo.destroy
+    flash[:notice] = "Photo went missing in action!"
+    redirect_to albums_url
+  end
+
+  def _find_album
+    @album = Album.find(params[:album_id])
+  end
+end
---
+++ app/models/photo.rb
@@ -0,0 +1,13 @@
+class Photo < ActiveRecord::Base
+  belongs_to :album
+  
+  has_attachment :content_type => :image,
+                 :storage => :file_system,
+                 :min_size => 0.kilobytes,
+                 :max_size => 1024.kilobytes,
+                 :rezise_to => '320x320>',
+                 :thumbnails => { :thumb => '64x64>' }
+                 
+  validates_as_attachment
+  
+end
